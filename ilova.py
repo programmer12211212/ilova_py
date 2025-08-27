3561099a51555ea8bc3b11a3ea09999cc08f62df
programmer12211212 <uchqunbekfarxodov003@gmail.com>
2025-08-27 08:27:18 +0500

    2-bosqich

--- ilova.py
+++ ilova.py
@@ -1,19 +1,27 @@
-def menyu():
-    print("\n--- Vazifa Ilova ---")
+
+topshiriq = []
+
+def menu():
     print("1 - Vazifa qo‘shish")
     print("2 - Ro‘yxatni ko‘rish")
     print("3 - Vazifani o‘chirish")
     print("0 - Chiqish")
 
-def main():
-    while True:
-        menyu()
-        tanlov = input("Tanlovni kiriting: ")
-        if tanlov == "0":
-            print("Dasturdan chiqildi.")
-            break
-        else:
-            print("Bu funksiya hali ishlamaydi.")
+while True:
+    menu()
+    tanlov = input("Tanlang: ")
 
-if __name__ == "__main__":
-    main()
+    if tanlov == "1":
+        vazifa = input("yangi vazifa kiriting: ")
+        topshiriq.append(vazifa)
+        print("vazifa qo‘shildi...")
+    elif tanlov == "2":
+        if not topshiriq:
+            print("xali vazifalar yoq")
+        else:
+            print("\n--- Vazifalar ro‘yxati ---")
+            for i, topshiriq in enumerate(topshiriq, start=1):
+                print(f"{i}. {topshiriq}")
+    elif tanlov == "0":
+        print("dastur tugadi")
+        break
