a96a6ccf4054d5bc754c86a980d0ef81d925e783
programmer12211212 <uchqunbekfarxodov003@gmail.com>
2025-08-27 08:32:12 +0500

    4-bosqich

--- ilova.py
+++ ilova.py
@@ -1,10 +1,25 @@
 topshiriq = []
 
+def yuklash():
+    try:
+        with open("malumotlar.txt", "r", encoding="utf-8") as f:
+            for qator in f:
+                topshiriq.append(qator.strip())
+    except FileNotFoundError:
+        pass
+
+def saqlash():
+    with open("malumotlar.txt", "w", encoding="utf-8") as f:
+        for t in topshiriq:
+            f.write(t + "\n")
+
 def menu():
     print("1 - Vazifa qo‘shish")
     print("2 - Ro‘yxatni ko‘rish")
     print("3 - Vazifani o‘chirish")
     print("0 - Chiqish")
+
+yuklash()
 
 while True:
     menu()
@@ -18,14 +33,14 @@ while True:
         if not topshiriq:
             print("xali vazifalar yoq")
         else:
-            print("\n--- Vazifalar ro‘yxati ---")
+            print("Vazifalar ro‘yxati:")
             for i, t in enumerate(topshiriq, start=1):
                 print(f"{i}. {t}")
     elif tanlov == "3":
         if not topshiriq:
             print("xali vazifalar yoq, o‘chirish mumkin emas")
         else:
-            print("Vazifalar ro‘yxati:")
+            print("\n--- Vazifalar ro‘yxati ---")
             for i, t in enumerate(topshiriq, start=1):
                 print(f"{i}. {t}")
             try:
@@ -38,5 +53,6 @@ while True:
             except ValueError:
                 print("iltimos, faqat raqam kiriting")
     elif tanlov == "0":
+        saqlash()
         print("dastur tugadi")
         break
